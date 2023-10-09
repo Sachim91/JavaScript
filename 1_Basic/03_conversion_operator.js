e6b0360b378fe374ad3087eac6fded7ea7695da0
Sachin <92576557+Sachim91@users.noreply.github.com>
2023-10-09 16:10:58 +0000

    basic

---
+++ 1_Basic/03_conversion_operator.js
@@ -0,0 +1,49 @@
+/*
+let score = "33abc";
+console.log(typeof score);
+console.log(typeof (score));
+
+let valueInNumber = Number(score);
+console.log(typeof valueInNumber);
+
+console.log(valueInNumber);
+
+
+//'33' =>33
+//"33abc"  =>NaN
+//true => 1; false => 0
+
+let subNumber = 33;
+let stringNumber = String(subNumber);
+console.log(stringNumber);
+console.log(typeof stringNumber);
+
+******************************Operation **********************
+
+let value = 3;
+let negValue = -value;
+console.log(negValue);
+
+
+
+let str = "hitesh";
+let str2 = " hello";
+let str3 = str + str2;
+console.log(str3);
+
+console.log(1 + "2");
+console.log(1 + 2 + "2");
+console.log(("2"+2+2));
+
+console.log((3 + 4) * 5 % 3);
+
+*/
+
+let num1,num2,num3
+
+num1 = num2 = num3 = 2 + 2;  //   not good
+
+
+let gameCounter = 100;
+//gameCounter++;
+console.log(gameCounter++);   //PREFIX AND POSTFIX
