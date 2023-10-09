06dd7bcbeeafa0acd0c49485a494d7a1c07d7e96
Sachin <92576557+Sachim91@users.noreply.github.com>
2023-10-09 15:35:18 +0000

    basic

---
+++ 1_Basic/02_dataType.js
@@ -0,0 +1,26 @@
+"use strict ";  // Treat all js code as newer version 
+
+// alert(3 + 3);   We are using nodejs , not browser
+
+console.log(3 + 3);
+console.log("Sachin"); //code readability should be high
+
+let name = "SACHIN";
+let age = 18;
+let isLoggedIn = false;
+
+//number => 2 to power 53
+//bigint 
+//string  =""
+//boolean => true/false
+//null -->it is standalone value,it is representation of value;
+
+//undefined
+//symbol =>unique
+
+//object 
+
+console.log(typeof age);   //number
+console.log(typeof null);   ///obj
+console.log(typeof undefined);  //undefined
+
