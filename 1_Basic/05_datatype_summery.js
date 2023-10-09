a386ea49d2c94b0400eb88f35ff7818e82126898
Sachin <92576557+Sachim91@users.noreply.github.com>
2023-10-09 16:40:33 +0000

    basic

---
+++ 1_Basic/05_datatype_summery.js
@@ -0,0 +1,41 @@
+//Primitive 
+/*
+// 7  type : String, Number, Boolean, null, undefined, Symbol ,BigInt
+const score = 100;
+const scoreValue = 100.3;
+const temp = null;
+let c = score;
+c = 25;
+console.log(c)
+console.log(score)
+
+//reference data type (Non Primitive)
+//Array, Object,Functions
+
+const heros = ["shaktiman", "naagraj", "devv"];
+let myObj = {
+    name: "hitesh",
+    age: 22, 
+
+}
+
+const myFunction = function () {
+    console.log("Hello World");
+}
+console.log(typeof temp);
+console.log(myFunction);
+console.log(myObj);
+console.log(heros);
+
+let obj = myObj;
+console.log(obj);
+obj.name = "Sachin";
+console.log(obj);
+console.log(myObj);
+
+
+//Stack and heap memory
+*/
+
+
+
